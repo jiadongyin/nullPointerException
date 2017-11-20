102ffcf0c2f636481c3f59f3c52fd865aa6c9fda
jiadongyin <jiadongkakaxi@live.com>
2017-11-20 14:30:14 +0800

    Add files via upload

---
+++ SystemConstant.java
@@ -0,0 +1,27 @@
+package com.hunt.util;
+
+/**
+ * @Author: ouyangan
+ * @Date : 2017-1-16 15:18:00
+ * @Description 系统常量
+ */
+public class SystemConstant {
+    public static final String shiro_cache_prefix = "shiro-cache-";
+    public static final String shiro_cache_prefix_keys = "shiro-cache-*";
+    public static final String github_client_id = "**";
+    public static final String github_client_secret = "**";
+    public static final String github_oauth_url = "**";
+
+    //阿里大于短信发送url
+    public static final String aLiDaYuRegisterUrl = "aLiDaYuRegisterUrl";
+    //阿里大于短信注册key
+    public static final String aLiDaYuRegisterAppKey = "aLiDaYuRegisterAppKey";
+    //阿里大于短信注册密钥
+    public static final String aLiDaYuRegisterSecret = "aLiDaYuRegisterSecret";
+    //阿里大于短信注册模板签名
+    public static final String aLiDaYuRegisterTemplateSign = "aLiDaYuRegisterTemplateSign";
+    //阿里大于短信模板编码
+    public static final String aLiDaYuRegisterTemplateCode = "aLiDaYuRegisterTemplateCode";
+
+
+}
